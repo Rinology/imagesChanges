15f48e6f2ec77a6907dba7152d43872ace4ed85f
taerin <taerin@qualisports.kr>
2026-06-08 21:35:14 +0900

    시작시 ui크기 변경

--- image_converter_ui.py
+++ image_converter_ui.py
@@ -10,7 +10,7 @@ class ImageConverterApp:
     def __init__(self, root):
         self.root = root
         self.root.title("이미지 변환 & 이름 변경 프로그램 v2 (WebP)")
-        self.root.geometry("1000x800")
+        self.root.geometry("1200x1000")
         
         self.selected_folder = ""
         self.image_files = []
